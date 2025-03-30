baefc5f0aba213ff6f6954de713b0525c1ff07e6
dimfu <dimmfu@gmail.com>
2025-03-30 22:22:17 +0800

    replace dog water tree node with naive tree component

--- frontend/src/components/FileTreeNode.vue
+++ frontend/src/components/FileTreeNode.vue
@@ -1,28 +1,46 @@
 <script lang="ts" setup>
+import type { TreeOption } from "naive-ui";
+import { NTree } from "naive-ui";
 import { main } from "../../wailsjs/go/models";
+
 const props = defineProps<{ dirNode: main.dirNode }>();
 const emit = defineEmits(["file-click"]);
+
+const createData = (node: main.dirNode, k: string): Array<TreeOption> => {
+  if (!node) return [];
+
+  const files = node.files.map((file) => ({
+    label: file,
+    key: file,
+    isLeaf: true,
+    children: [],
+  }));
+
+  const subdirs = Object.entries(node.subdirs || {}).map(([name, subdir]) => ({
+    label: name,
+    key: `${k}_${name}`,
+    children: createData(subdir, name),
+  }));
+
+  return [...subdirs, ...files];
+};
+
+const nodeProps = ({ option }: { option: TreeOption }) => {
+  return {
+    onClick() {
+      if (option.isLeaf) {
+        emit("file-click", option.label);
+      }
+    },
+  };
+};
 </script>
 
 <template>
-  <ul>
-    <li
-      v-for="(subdir, name) in props.dirNode!.subdirs"
-      :key="name"
-      @file-click="emit('file-click', $event)"
-    >
-      📂 {{ name }}
-      <FileTreeNode
-        @file-click="emit('file-click', $event)"
-        :dir-node="subdir"
-      />
-    </li>
-    <li
-      v-for="file in props.dirNode!.files"
-      :key="file"
-      @click="emit('file-click', file)"
-    >
-      📄 {{ file }}
-    </li>
-  </ul>
+  <n-tree
+    block-line
+    :data="createData(props.dirNode, 'root')"
+    :node-props="nodeProps"
+    expand-on-click
+  />
 </template>
